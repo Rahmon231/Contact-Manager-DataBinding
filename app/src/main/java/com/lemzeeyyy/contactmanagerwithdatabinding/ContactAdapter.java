faa464355631c1f17340ec2edc614a3c90e6ee11
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-02 23:31:49 +0100

    adapter

---
+++ app/src/main/java/com/lemzeeyyy/contactmanagerwithdatabinding/ContactAdapter.java
@@ -0,0 +1,31 @@
+package com.lemzeeyyy.contactmanagerwithdatabinding;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.MyViewHolder> {
+    @NonNull
+    @Override
+    public ContactAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
